eb6d29be6e908b3a32077d9c5fb90d799cd11ec0
Dylan Awalt-Conley <web@dylan.ac>
2022-03-19 22:19:40 -0400

    finalized functions and npm scripts to create and update stack from files

--- index.js
+++ index.js
@@ -1,39 +1,42 @@
-import * as fs from 'fs';
+import { promises as fsp } from 'fs';
 import * as path from 'path';
-import { globbySync } from 'globby';
+import { globby } from 'globby';
 
-const functions = globbySync('functions/*.js').map(file => ({
-    name: path.parse(file).name,
-    code: fs.readFileSync(file).toString(),
-}));
+export default async () => {
+    let functions = await globby('functions/*.js');
+    functions = functions.map(async file => {
+        const name = path.parse(file).name;
+        const code = (await fsp.readFile(file)).toString();
+        return { name, code };
+    });
+    functions = await Promise.all(functions);
 
-const stack = {
-    AWSTemplateFormatVersion: '2010-09-09',
-    Description: 'Controls URL cleaning and subdomain redirection.',
-    Resources: functions.reduce((obj, {name, code}) => ({
-        ...obj,
-        [name]: {
-            Type: 'AWS::CloudFront::Function',
-            Properties: {
-                Name: name,
-                AutoPublish: true,
-                FunctionConfig: {
-                    Comment: '',
-                    Runtime: 'cloudfront-js-1.0',
+    return {
+        AWSTemplateFormatVersion: '2010-09-09',
+        Description: 'Controls URL cleaning and subdomain redirection.',
+        Resources: functions.reduce((obj, {name, code}) => ({
+            ...obj,
+            [name]: {
+                Type: 'AWS::CloudFront::Function',
+                Properties: {
+                    Name: name,
+                    AutoPublish: true,
+                    FunctionConfig: {
+                        Comment: '',
+                        Runtime: 'cloudfront-js-1.0',
+                    },
+                    FunctionCode: code,
                 },
-                FunctionCode: code,
             },
-        },
-    }), {}),
-    Outputs: functions.reduce((obj, {name}) => ({
-        ...obj,
-        [name + 'Arn']: {
-            Value: { 'Fn::GetAtt': `${name}.FunctionMetadata.FunctionARN`, },
-            Export: { 
-                Name: { 'Fn::Sub': '${AWS::StackName}-' + name },
+        }), {}),
+        Outputs: functions.reduce((obj, {name}) => ({
+            ...obj,
+            [name + 'Arn']: {
+                Value: { 'Fn::GetAtt': `${name}.FunctionMetadata.FunctionARN`, },
+                Export: { 
+                    Name: { 'Fn::Sub': '${AWS::StackName}-' + name },
+                },
             },
-        },
-    }), {}),
+        }), {}),
+    };
 };
-
-fs.writeFileSync('template.json', JSON.stringify(stack, null, 2));
---
+++ update-stack.js
@@ -0,0 +1,36 @@
+import { promises as fsp } from 'fs';
+import makeStack from './index.js';
+import { CloudFormationClient, CreateStackCommand, UpdateStackCommand } from '@aws-sdk/client-cloudformation';
+
+const mode = process.argv[2];
+
+let [ config, stack ] = await Promise.all([
+    fsp.readFile('config.json').then(JSON.parse),
+    makeStack(),
+]);
+
+if (mode === '--file') {
+    const stackFile = process.argv[3] || 'template.json';
+    await fsp.writeFile(stackFile, JSON.stringify(stack, null, 2));
+} else {
+    const client = new CloudFormationClient({ region: 'us-east-1' });
+    const commandInput = {
+        StackName: config.StackName,
+        TemplateBody: JSON.stringify(stack),
+    };
+
+    let command;
+    if (mode === '--create') {
+        command = new CreateStackCommand(commandInput);
+    } else {
+        command = new UpdateStackCommand(commandInput);
+    }
+
+    let result;
+    try {
+        result = await client.send(command);
+    } catch (e) {
+        console.error(e);
+    }
+    console.log(result);
+}
